01653f16cb594c71d69642abae9053ef0f246b4b
b-forsythe <business.brandonforsythe@gmail.com>
2024-12-31 05:42:52 -0900

    added small features, question mark in home screen, prep for final

---
+++ CaCl2Manual/CaCl_Calculator.swift
@@ -0,0 +1,18 @@
+//
+//  CaCl_Calculator.swift
+//  CaCl2Manual
+//
+//  Created by Brandon Forsythe on 7/22/24.
+//
+
+import SwiftUI
+
+struct CaCl_Calculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaCl_Calculator()
+}
